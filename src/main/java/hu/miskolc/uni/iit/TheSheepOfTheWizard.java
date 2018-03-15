1d79fa170a1cd33e981d0dc62d56a7e12a65bea9
Alex Toth <tothalex95@gmail.com>
2018-03-15 18:46:56 +0100

    Added main file & resources.

---
+++ src/main/java/hu/miskolc/uni/iit/TheSheepOfTheWizard.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package hu.miskolc.uni.iit;
+
+import java.io.IOException;
+
+/**
+ * @author Alex Toth
+ *
+ */
+public class TheSheepOfTheWizard {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
